4eebefe4bb874d22460fa7f13c65493c2f059e30
Andy <53950662+AdrianRazor@users.noreply.github.com>
2023-09-21 21:28:24 +0300

    added adaptive

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,59 @@
 document.addEventListener("DOMContentLoaded", function (event) {
+  const root = document.documentElement;
 
+  // Header
+  const header = document.querySelector(".header");
+  const burger = header.querySelector(".burger");
 
+  if (header && burger) {
+    burger.addEventListener("click", () => {
+      header.classList.toggle("open");
+      burger.classList.toggle("open");
+      root.classList.toggle("lock");
+    });
+  }
 
-  
+  // Coins buy/sell
+  const coinBox = document.querySelectorAll(".start__coins");
+  const coinListBox = document.querySelectorAll(".start__list");
+
+  if (coinBox && coinListBox) {
+    coinBox.forEach((box, boxIndex) => {
+      const coinBtn = box.querySelectorAll(".btn--small");
+
+      if (coinBtn) {
+        coinBtn.forEach((btn, btnIndex) => {
+          btn.addEventListener("click", () => {
+            const itemsList =
+              coinListBox[boxIndex].querySelectorAll(".start__item");
+
+            itemsList.forEach((el) => el.classList.remove("active"));
+            coinBtn.forEach((el) => el.classList.remove("active"));
+
+            btn.classList.add("active");
+            itemsList[btnIndex].classList.add("active");
+          });
+        });
+      }
+    });
+    coinListBox.forEach((box, boxIndex) => {
+      const coinItem = box.querySelectorAll(".start__item");
+
+      if (coinItem) {
+        coinItem.forEach((btn, btnIndex) => {
+          btn.addEventListener("click", () => {
+            const coinBtn = coinBox[boxIndex].querySelectorAll(".btn--small");
+
+            coinBtn.forEach((el) => el.classList.remove("active"));
+            coinItem.forEach((el) => el.classList.remove("active"));
+
+            btn.classList.add("active");
+            coinBtn[btnIndex].classList.add("active");
+          });
+        });
+      }
+    });
+  }
+
   console.log("DOM fully loaded and parsed");
 });
